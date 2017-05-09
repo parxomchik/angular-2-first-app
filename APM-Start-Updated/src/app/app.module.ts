1b13582620df9143eeba912353dd506603ae5d02
Dmitriy Parkhomenko <parkhomenko@MacBook-Pro-Dmitriy.local>
2017-05-09 23:20:08 +0300

    added angular form module

--- APM-Start-Updated/src/app/app.module.ts
+++ APM-Start-Updated/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { ProductListComponent } from './products/product-list.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, FormsModule ],
   declarations: [
     AppComponent,
     ProductListComponent
