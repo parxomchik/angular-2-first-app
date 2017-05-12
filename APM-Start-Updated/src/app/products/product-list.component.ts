09704d10f122b388f859ab6d1a1de6e35a65145e
Dmitriy Parkhomenko <parkhomenko@MacBook-Pro-Dmitriy.local>
2017-05-12 16:13:02 +0300

    added Observable for products service

--- APM-Start-Updated/src/app/products/product-list.component.ts
+++ APM-Start-Updated/src/app/products/product-list.component.ts
@@ -15,6 +15,7 @@ export class ProductListComponent implements OnInit {
     imageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
+    errorMessage: string;
     products: IProduct[];
 
     constructor(private _productService: ProductService) {
@@ -26,7 +27,11 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this._productService.getProducts();
+        this._productService.getProducts()
+            .subscribe(
+                products => this.products = products,
+                error => this.errorMessage = <any>error
+            );
     }
 
     onRatingClicked(message: string): void {
