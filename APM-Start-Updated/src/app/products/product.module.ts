f9cb271a902791e208b4ad2862a8f71be4b8a7e1
Dmitriy Parkhomenko <parkhomenko@MacBook-Pro-Dmitriy.local>
2017-05-14 19:02:01 +0300

    added shater module

--- APM-Start-Updated/src/app/products/product.module.ts
+++ APM-Start-Updated/src/app/products/product.module.ts
@@ -1,25 +1,23 @@
 import { ProductService } from './product.service';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-import { StarComponent } from './../shared/star.component';
 import { ProductFilterPipe } from './product-filter.pipe';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailGuard } from './product-guard.service';
 
+import { SharedModule } from './../shared/shared.module';
+
+
 @NgModule({
     declarations: [
         ProductListComponent,
         ProductDetailComponent,
-        ProductFilterPipe,
-        StarComponent
+        ProductFilterPipe
     ],
     imports: [
-        CommonModule,
-        FormsModule,
+        SharedModule,
         RouterModule.forChild([
             { path: 'products', component: ProductListComponent },
             { path: 'product/:id',
